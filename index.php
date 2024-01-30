891a27e662cf2a82b09dfeaa136cb90f835bd3e9
xJustFox <dodydaversa@gmail.com>
2024-01-30 14:10:12 +0100

    [Upgrade] Added function setDuration

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
     class Movie{
         public $name;
         public $genre;
-        public $duration;
+        public $duration = 0;
         public $year;
 
-        function __construct($_name, $_genre, $_duration, $_year){
+        function __construct($_name, $_genre, $_year){
             $this->name = $_name;
             $this->genre = $_genre;
-            $this->duration = $_duration;
             $this->year = $_year;
+        }
+
+        public function setDutarion($time){
+            $this->duration = gmdate('H:i', ($time * 60));
         }
     }
 
     // istanza classe Movie
-    $movie_1 = new Movie("Matrix","Fantascienza",136,1999);
+    $movie_1 = new Movie("Matrix","Fantascienza", 1999);
+    $movie_1->setDutarion(136);
 
     var_dump($movie_1);
 ?>
