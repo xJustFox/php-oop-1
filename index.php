f82bda99fe513afdf76362960b6c99b05a5a59c4
xJustFox <dodydaversa@gmail.com>
2024-01-30 16:45:37 +0100

    [Fix] Fixed setDuration function

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             $this->year = $_year;
         }
 
-        public function setDutarion($time){
+        public function setDuration($time){
             $this->duration = gmdate('H:i', ($time * 60));
         }
 
@@ -23,22 +23,22 @@
 
     // istanze classe Movie
     $movie_1 = new Movie("Matrix", ["Fantascienza", "Azione", "Thriller", "Fantascienza"], 1999);
-    $movie_1->setDutarion(136);
+    $movie_1->setDuration(136);
 
     $movie_2 = new Movie("V per Vendetta", ["Azione", "Drammatico", "Thriller", "Fantascienza"], 2005);
-    $movie_2->setDutarion(132);
+    $movie_2->setDuration(132);
 
     $movie_3 = new Movie("Watchmen", ["Azione", "Drammatico", "Fantascienza", "Thriller"], 2009);
-    $movie_3->setDutarion(163);
+    $movie_3->setDuration(163);
 
     $movie_4 = new Movie("Daredevil", ["Azione", "Drammatico", "Thriller"], 2003);
-    $movie_4->setDutarion(133);
+    $movie_4->setDuration(133);
 
     $movie_5 = new Movie("Spider-Man 3", ["Azione", "Fantascienza", "Drammatico"], 2007);
-    $movie_5->setDutarion(139);
+    $movie_5->setDuration(139);
 
     $movie_6 = new Movie("John Wick", ["Azione", "Thriller"], 2014);
-    $movie_6->setDutarion(161);
+    $movie_6->setDuration(161);
 
     $arr_movie = [$movie_1, $movie_2, $movie_3, $movie_4, $movie_5, $movie_6];
 ?>
